fde454cf52b17eb87e3d909b842c68e0635824ba
“zhangchao22 <18910146715@163.com>
2020-07-22 17:32:35 +0800

    证明枚举单例模式的安全性

---
+++ singleton/Singleton8Pro.java
@@ -0,0 +1,30 @@
+package com.imooc.zhangxiaoxi.alllearning.singleton;
+
+/**
+ * 描述：     证明枚举单例模式的安全性
+ */
+public enum Singleton8Pro {
+    INSTANCE(new Object());
+
+    //单例对象
+    private Object obj;
+
+    Singleton8Pro(Object obj) {
+        this.obj = obj;
+    }
+
+    public void whatever() {
+        System.out.println(Singleton8Pro.INSTANCE.obj);
+    }
+
+
+    public static void main(String[] args) {
+         for (int i=1; i<=2000; i++)
+          {
+              new  Thread(() -> {
+                  Singleton8Pro.INSTANCE.whatever();
+             },String.valueOf(i)).start();
+         }
+    }
+
+}
